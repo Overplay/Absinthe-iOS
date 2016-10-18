dab82b511a6de0b6f29a338a5109d295b87248a3
atorres <torres.lyssa@gmail.com>
2016-10-18 10:01:52 -0700

    adding removed controller

--- EditAccountViewController.swift
+++ EditAccountViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class EditAccountViewController: UIViewController {
 
@@ -18,6 +19,21 @@ class EditAccountViewController: UIViewController {
         self.navigationController!.popViewControllerAnimated(true)
     }
     
+    @IBAction func save(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Save Changes", message: "Are you sure you want to save changes to your account information?", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "No", style: .Cancel) { (action) in }
+        
+        alertController.addAction(cancelAction)
+        
+        // TODO: change account information with call to Asahi and store new info in Settings
+        let okAction = UIAlertAction(title: "Yes", style: .Default) { (action) in }
+        
+        alertController.addAction(okAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,6 +43,38 @@ class EditAccountViewController: UIViewController {
         self.lastName.text = Settings.sharedInstance.userLastName
    
         self.email.text = Settings.sharedInstance.userEmail
+        
+        Asahi.sharedInstance.checkAuthStatus()
+            
+            .then{ response -> Void in
+                log.debug("Successfully checked auth")
+                if let first = response["firstName"].string {
+                    self.firstName.text = first
+                }
+                
+                if let last = response["lastName"].string {
+                    self.lastName.text = last
+                }
+                
+                if let email = response["auth"]["email"].string {
+                    self.email.text = email
+                }
+            }
+            
+            // TODO: how do we want to handle this error?
+            .error{ err -> Void in
+                log.error("Error checking auth")
+                print(err)
+                
+                let alertController = UIAlertController(title: "Error loading account information", message: "There was an issue loading your account information.", preferredStyle: .Alert)
+                
+                let okAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+                }
+                
+                alertController.addAction(okAction)
+                self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
