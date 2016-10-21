26bc4c5b4cda819f0a7ab81aad1695a1b917c8dd
atorres <torres.lyssa@gmail.com>
2016-10-20 18:14:34 -0700

    changes to change password page

--- ChangePasswordViewController.swift
+++ ChangePasswordViewController.swift
@@ -20,6 +20,8 @@ class ChangePasswordViewController: AccountBaseViewController {
     @IBOutlet weak var newPasswordCheck: UIImageView!
     @IBOutlet weak var repeatNewPasswordCheck: UIImageView!
     
+    @IBOutlet weak var saveButton: UIButton!
+    
     @IBAction func goBack(sender: AnyObject) {
         self.navigationController!.popViewControllerAnimated(true)
     }
@@ -46,12 +48,14 @@ class ChangePasswordViewController: AccountBaseViewController {
         self.passwordCheck.alpha = 0
         self.newPasswordCheck.alpha = 0
         self.repeatNewPasswordCheck.alpha = 0
+        self.saveButton.alpha = 0
         
         
         self.email.text = Settings.sharedInstance.userEmail
-        checkEmail()
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(checkEmail), name: UITextFieldTextDidChangeNotification, object: nil)
+        checkInputs()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(checkInputs), name: UITextFieldTextDidChangeNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -59,18 +63,64 @@ class ChangePasswordViewController: AccountBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func checkEmail() {
+    func checkInputs() {
+        if checkEmail() &&
+            checkPassword(self.currentPassword, checkImage: self.passwordCheck) &&
+            checkPassword(self.newPassword, checkImage: self.newPasswordCheck) &&
+            checkPassword(self.repeatNewPassword, checkImage: self.repeatNewPasswordCheck) {
+            
+            fadeIn(self.saveButton)
+        }
         
+        else {
+            fadeOut(self.saveButton)
+        }
+    }
+    
+    func checkEmail() -> Bool {
         if let email = self.email.text {
             
             if email.isValidEmail() && self.emailCheck.alpha == 0 {
                 fadeIn(self.emailCheck)
+                return true
             }
             
             if !email.isValidEmail() {
                 fadeOut(self.emailCheck)
+                return false
+            }
+            
+            else {
+                return true
             }
         }
+        
+        return false
+    }
+    
+    func checkPassword(textField: UITextField, checkImage: UIImageView) -> Bool {
+        if let pwd = textField.text {
+            
+            if pwd.isValidPwd() && checkImage.alpha == 0 {
+                fadeIn(checkImage)
+                return true
+            }
+            
+            if !pwd.isValidPwd() {
+                fadeOut(checkImage)
+                return false
+            }
+            
+            else {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func checkRepeatPassword() -> Bool {
+        return true
     }
 
     /*
